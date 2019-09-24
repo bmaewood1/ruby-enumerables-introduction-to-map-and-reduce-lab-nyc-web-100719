5099d5c356b37a70fb3a4e1cebaafd06d1f48468
Bailey Wood <woodbmae@gmail.com>
2019-09-24 14:20:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,23 +19,13 @@ def map_to_square(source_array)
 end
 
 
-def reduce_to_total(source_array, starting_point = nil, &block)
-  if starting_point.nil?
-    elements.reduce(&block)
-  else
-    elements.reduce(starting_point, &block)
-  end
-end
-  
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   source_array.reduce(starting_point) { |sum, n| sum + n }
 end
 
 def reduce_to_all_true(source_array)
   source_array.reduce { |beTrue, beFalse| beTrue && beFalse }
 end
-
-
 
 def reduce_to_any_true(source_array)
   source_array.any?
