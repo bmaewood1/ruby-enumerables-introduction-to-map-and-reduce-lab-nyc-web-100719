4038132648c704c6f73814541c5d34b492a0115c
Bailey Wood <woodbmae@gmail.com>
2019-09-24 13:32:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,3 +4,13 @@ def map_to_negativize(source_array)
   source_array.map { |n| -n }
 end
 
+describe "map_to_no_change returns an array with the original values" do
+    it "transforms correctly" do
+      dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
+      expect(map_to_no_change(dune)).to eq(dune)
+    end
+  end
+  
+def map_to_no_change(source_array)
+  source_array
+end
