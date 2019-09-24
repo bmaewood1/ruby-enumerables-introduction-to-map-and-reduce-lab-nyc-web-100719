295672c2069d815542baafc92eef73f859a39612
Bailey Wood <woodbmae@gmail.com>
2019-09-24 13:31:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,17 +1,4 @@
 # My Code here....
-describe 'my own map-like methods' do
-  describe "map_to_negativize returns an array with all values made negative" do
-    it "transforms correctly" do
-      expect(map_to_negativize([1, 2, 3, -9])).to eq([-1, -2, -3, 9])
-    end
-  end
-
-  describe "map_to_no_change returns an array with the original values" do
-    it "transforms correctly" do
-      dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
-      expect(map_to_no_change(dune)).to eq(dune)
-    end
-  end
 
 def map_to_negativize(source_array)
   source_array.map { |n| -n }
