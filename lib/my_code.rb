f19a8a4487776da8454ea2121fe61728f45700e5
Bailey Wood <woodbmae@gmail.com>
2019-09-24 14:07:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,39 +27,9 @@ def reduce_to_total(source_array, starting_point)
   source_array.reduce(starting_point) { |sum, n| sum + n }
 end
 
- describe "reduce_to_all_true returns true when all values are truthy" do
-    it "reduces correctly" do
-      source_array = [1, 2, true, "razmatazz"]
-      expect(reduce_to_all_true(source_array)).to be_truthy
-    end
-  end
 
-def reduce_to_all_true(source_array)
-  source_array
+def reduce_to_all_true(source_array, starting_point = 0, &block)
+  source_array.reduce(starting_point, &block){ |x, y| if x || y = true; p true; else p false; end}
 end
-
-  describe "reduce_to_all_true returns false when any value is false" do
-    it "reduces correctly" do
-      source_array = [1, 2, true, "razmatazz", false]
-      expect(reduce_to_all_true(source_array)).to be_falsy
-    end
-  end
-
-def reduce_to_all_true(source_array)
-  source_array.reduce(starting_point, &block){ |x, y| if x || y = true; p true; else p false
   
 
-  describe "reduce_to_any_true returns true when a truthy value is present" do
-    it "reduces correctly" do
-      source_array = [ false, nil, nil, nil, true]
-      expect(reduce_to_any_true(source_array)).to eq(true)
-    end
-  end
-
-  describe "reduce_to_any_true returns false when no truthy value is present" do
-    it "reduces correctly" do
-      source_array = [ false, nil, nil, nil]
-      expect(reduce_to_any_true(source_array)).to eq(false)
-    end
-  end
-end
