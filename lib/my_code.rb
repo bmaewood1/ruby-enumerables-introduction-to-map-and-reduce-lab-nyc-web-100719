609c48df7bd833a073fbf4ec4c60095070b78880
Bailey Wood <woodbmae@gmail.com>
2019-09-24 13:40:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,19 +18,10 @@ def map_to_square(source_array)
   source_array.map { |n| n * n }
 end
 
-describe 'my own reduce-like methods' do
-  describe "reduce_to_total returns a running total when not given a starting point" do
-    it "reduces correctly" do
-      source_array = [1,2,3]
-      expect(reduce_to_total(source_array)).to eq(6)
-    end
-  end
 
 def reduce_to_total(source_array)
-
-
-
-
+  source_array.reduce(0) { |sum, num| sum + num }
+end
 
   describe "reduce_to_total returns a running total when given a starting point" do
     it "reduces correctly" do
@@ -39,3 +30,4 @@ def reduce_to_total(source_array)
       expect(reduce_to_total(source_array, starting_point)).to eq(106)
     end
   end
+  
