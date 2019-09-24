3ebea6ca5a55b94bef3a5c76db930b7ba806bbde
Bailey Wood <woodbmae@gmail.com>
2019-09-24 14:09:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,8 +19,12 @@ def map_to_square(source_array)
 end
 
 
-def reduce_to_total(source_array)
-  source_array.reduce(:+)
+def reduce_to_total(source_array, starting_point = nil, &block)
+  if starting_point.nil?
+    elements.reduce(&block)
+  else
+    elements.reduce(starting_point, &block)
+  end
 end
   
 def reduce_to_total(source_array, starting_point)
